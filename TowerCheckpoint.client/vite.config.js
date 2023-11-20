71ca996ac6f9c31ba7c06914a1bbcd0568d81e4d
Tyler Marcott <tylerjmarcott@gmail.com>
2023-11-20 12:26:01 -0700

    rename commit

--- TowerCheckpoint.client/vite.config.js
+++ TowerCheckpoint.client/vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'tower',
   build: {
     outDir: '../server/www',
     sourcemap: false,

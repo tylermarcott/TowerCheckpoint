68b8f98348d65d807c08de97f927413d692155a5
Tyler Marcott <tylerjmarcott@gmail.com>
2023-11-22 11:13:19 -0700

    env change push

--- client/src/env.js
+++ client/src/env.js
@@ -1,7 +1,7 @@
 export const dev = window.location.origin.includes('localhost')
 
 // NOTE: change the http://localhost to http://tylermarcott.dev/tower
-export const baseURL = dev ? 'http://localhost:3000' : '/tower'
+export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
 export const domain = 'dev-r2l4kl7ifqatct23.us.auth0.com'
 export const clientId = '3ugFmbTisZI8MvM7ux0qRbSLZS8X8Zo1'
